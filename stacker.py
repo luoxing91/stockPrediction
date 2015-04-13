05330f466cb8ae933c62633f1a523875d0a1bbe8
luoxing <xingfe123@gmail.com>
2015-04-14 05:14:38 +0800

    chmod

--- stacker.py
+++ stacker.py
@@ -2,27 +2,15 @@
 # <nbformat>3.0</nbformat>
 
 # <codecell>
+import operator
+import sys
+from time import strftime
 
 import numpy as np
-import math
-from math import log
-from sklearn import metrics, preprocessing, cross_validation
-from sklearn.feature_extraction.text import TfidfVectorizer
-import sklearn.linear_model as lm
 import pandas as p
-from time import gmtime, strftime
-import scipy
-import sys
-import sklearn.decomposition
-from sklearn.metrics import mean_squared_error
-from string import punctuation
-from sklearn.neighbors import RadiusNeighborsRegressor, KNeighborsRegressor
-import time
-from scipy import sparse
-from matplotlib import *
-from itertools import combinations
-from sklearn.ensemble import GradientBoostingClassifier,RandomForestClassifier, ExtraTreesClassifier
-import operator
+import sklearn.linear_model as lm
+from sklearn.ensemble import RandomForestClassifier
+
 
 # <codecell>
 
@@ -52,11 +40,11 @@ def tied_rank(x):
     for i in range(len(sorted_x)):
         if cur_val != sorted_x[i][0]:
             cur_val = sorted_x[i][0]
-            for j in range(last_rank, i): 
+            for j in range(last_rank, i):
                 r[sorted_x[j][1]] = float(last_rank+1+i)/2.0
             last_rank = i
         if i == len(sorted_x)-1:
-            for j in range(last_rank, i+1): 
+            for j in range(last_rank, i+1):
                 r[sorted_x[j][1]] = float(last_rank+i+2)/2.0
     return r
 
@@ -64,7 +52,7 @@ def tied_rank(x):
 def auc(actual, posterior):
     """This function is by Ben Hamner and taken from
     https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/auc.py
-    
+
     Computes the area under the receiver-operater characteristic (AUC)
 
     This function computes the AUC error metric for binary classification.
@@ -97,17 +85,16 @@ def auc_scorer(estimator, X, y):
     predicted = estimator.predict_proba(X)[:, 1]
     return auc(y, predicted)
 
-                
+
 def normalize10day(stocks):
     def process_column(i):
         if operator.mod(i, 5) == 4:
             return np.log(stocks[:, i]+1)
         else:
             return stocks[:, i] / stocks[:, 0]
-    n = stocks.shape[0]
     stocks_dat = np.array([process_column(i) for i in range(31)]).transpose()
     return stocks_dat
-    
+
 
 # <codecell>
 
@@ -161,10 +148,12 @@ for i in range(10):
     indxs_to_fit = list(set(range(X.shape[0])) -
                         set(np.arange(i, X.shape[0], 10)))
     pred_ridge = pred_ridge+list(model_ridge
-                                 .fit(X[indxs_to_fit, :],
-                                      y[indxs_to_fit, :]).predict_proba(X[indxs, :])[:, 1])
-    
-    pred_randomforest = pred_randomforest + list(model_randomforest.fit(X[indxs_to_fit, :], y[indxs_to_fit, :]).predict_proba(X[indxs, :])[:, 1])         
+                                 .fit(X[indxs_to_fit, :], y[indxs_to_fit, :])
+                                 .predict_proba(X[indxs, :])[:, 1])
+
+    pred_randomforest += list(model_randomforest
+                              .fit(X[indxs_to_fit, :], y[indxs_to_fit, :])
+                              .predict_proba(X[indxs, :])[:, 1])
     new_Y = new_Y + list(y[indxs, :])
 
 new_X = np.hstack((np.array(pred_ridge).reshape(len(pred_ridge), 1),
@@ -176,33 +165,39 @@ new_Y = np.array(new_Y).reshape(len(new_Y), 1)
 # <codecell>
 
 model_stacker = lm.LogisticRegression()
-print(np.mean(cross_validation.cross_val_score(model_stacker, new_X, new_Y.reshape(new_Y.shape[0]), cv=5, scoring = auc_scorer)))
+
 
 # <codecell>
 
 model_stacker.fit(new_X, new_Y.reshape(new_Y.shape[0]))
 
-print "prediction"
+print("prediction")
 # do a prediction and save it
-pred_ridge_test = model_ridge.fit(X, y).predict_proba(X_test)[:,1]
-pred_randomforest_test = model_randomforest.fit(X, y).predict_proba(X_test)[:,1]
+pred_ridge_test = model_ridge.fit(X, y).predict_proba(X_test)[:, 1]
+pred_randomforest_test = model_randomforest.fit(X, y).predict_proba(X_test)[:,
+                                                                            1]
 
-new_X_test = np.hstack((np.array(pred_ridge_test).reshape(len(pred_ridge_test), 1), np.array(pred_randomforest_test).reshape(len(pred_randomforest_test), 1)))
+new_X_test = np.hstack((np.array(pred_ridge_test)
+                        .reshape(len(pred_ridge_test), 1),
+                        np.array(pred_randomforest_test)
+                        .reshape(len(pred_randomforest_test), 1)))
 
 # <codecell>
 
-pred = model_stacker.predict_proba(new_X_test)[:,1]
-testfile = p.read_csv('./test.csv', sep=",", na_values=['?'], index_col=[0,1])
+pred = model_stacker.predict_proba(new_X_test)[:, 1]
+testfile = p.read_csv('./test.csv', sep=",", na_values=['?'], index_col=[0, 1])
 
 # submit as D multiplied by 100 + stock id
 testindices = [100 * D + StId for (D, StId) in testfile.index]
 
-pred_df = p.DataFrame(np.vstack((testindices, pred)).transpose(), columns=["Id", "Prediction"])
-pred_df.to_csv('./predictions/' + 'stacker' + '/' + 'stacker' + ' ' + strftime("%m-%d %X") + ".csv", index = False)
+pred_df = p.DataFrame(np.vstack((testindices, pred)).transpose(),
+                      columns=["Id", "Prediction"])
+
+pred_df.to_csv('./predictions/stacker/stacker' + strftime('%m-%d %X') + ".csv",
+               index=False)
 
 print("submission file created")
 
 # <codecell>
 
-stacker.coef_
-
+# stacker.coef_
--- tuner.py
+++ tuner.py
@@ -2,30 +2,15 @@
 # <nbformat>3.0</nbformat>
 
 # <codecell>
-
-import numpy as np
-import math
-from math import log
-from sklearn import metrics, preprocessing, cross_validation
-from sklearn.feature_extraction.text import TfidfVectorizer
-import sklearn.linear_model as lm
-import pandas as p
-from time import gmtime, strftime
-import scipy
-import sys
-import sklearn.decomposition
-from sklearn.metrics import mean_squared_error
-from string import punctuation
-from sklearn.neighbors import RadiusNeighborsRegressor, KNeighborsRegressor
-import time
-from scipy import sparse
-from matplotlib import *
-from itertools import combinations
-from sklearn.ensemble import GradientBoostingClassifier,
-RandomForestClassifier, ExtraTreesClassifier
-
 import operator
-from sklearn import svm
+import sys
+from time import strftime
+import numpy as np
+import pandas as p
+import sklearn.linear_model as lm
+from sklearn import cross_validation
+from sklearn.ensemble import RandomForestClassifier
+
 
 # <codecell>
 
@@ -55,11 +40,11 @@ def tied_rank(x):
     for i in range(len(sorted_x)):
         if cur_val != sorted_x[i][0]:
             cur_val = sorted_x[i][0]
-            for j in range(last_rank, i): 
+            for j in range(last_rank, i):
                 r[sorted_x[j][1]] = float(last_rank+1+i)/2.0
             last_rank = i
         if i == len(sorted_x)-1:
-            for j in range(last_rank, i+1): 
+            for j in range(last_rank, i+1):
                 r[sorted_x[j][1]] = float(last_rank+i+2)/2.0
     return r
 
@@ -71,7 +56,7 @@ def numberOfOnes(actual):
 def auc(actual, posterior):
     """This function is by Ben Hamner and taken from
     https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/auc.py
-    
+
     Computes the area under the receiver-operater characteristic (AUC)
 
     This function computes the AUC error metric for binary classification.
@@ -115,11 +100,10 @@ def normalize10day(stocks):
             return stocks[:, i] * 0
         else:
             return stocks[:, i] / stocks[:, 0]
-    n = stocks.shape[0]
+
     stocks_dat = np.array([process_column(i) for i in range(46)]).transpose()
 
     return stocks_dat
-    
 
 # <codecell>
 
@@ -134,15 +118,16 @@ test = np.array(p.read_table('./test.csv', sep=","))
 # READ IN THE TEST DATA
 ###############################################################################
 # all data from opening 1 to straight to opening 10
-X_test_stockdata = normalize10day(test[:,range(2, 48)]) # load in test data
-X_test_stockindicators = np.vstack((np.identity(94)[:,range(93)] for i in range(25)))
+X_test_stockdata = normalize10day(test[:, range(2, 48)])  # load in test data
+X_test_stockindicators = np.vstack((np.identity(94)[:, range(93)]
+                                    for i in range(25)))
 
 
 X_test = X_test_stockdata
 
 
 ###############################################################################
-# READ IN THE TRAIN DATA
+# read in the train
 ###############################################################################
 n_windows = 490
 windows = range(n_windows)
@@ -150,9 +135,9 @@ windows = range(n_windows)
 X_windows = [train[:, range(1 + 5*w, 47 + 5*w)] for w in windows]
 X_windows_normalized = [normalize10day(w) for w in X_windows]
 X_stockdata = np.vstack(X_windows_normalized)
-X_stockindicators = np.vstack((np.identity(94)[:, range(93)] for i in range(n_windows)))
+X_stockindicators = np.vstack((np.identity(94)[:, range(93)]
+                               for i in range(n_windows)))
 
-#X = np.hstack((X_stockindicators, X_stockdata))
 X = X_stockdata
 
 # read in the response variable
@@ -167,19 +152,19 @@ print("preparing models")
 
 modelname = "lasso"
 
-if modelname == "ridge": 
+if modelname == "ridge":
     C = np.linspace(300, 5000, num=10)[::-1]
     models = [lm.LogisticRegression(penalty="l2", C=c) for c in C]
 
-if modelname == "lasso": 
+if modelname == "lasso":
     C = np.linspace(300, 5000, num=10)[::-1]
     models = [lm.LogisticRegression(penalty="l1", C=c) for c in C]
 
-if modelname == "sgd": 
+if modelname == "sgd":
     C = np.linspace(0.00005, .01, num=5)
     models = [lm.SGDClassifier(loss="log", penalty="l2",
                                alpha=c, warm_start=False) for c in C]
-    
+
 if modelname == "randomforest":
     C = np.linspace(50, 300, num=10)
     models = [RandomForestClassifier(n_estimators=int(c)) for c in C]
@@ -204,7 +189,7 @@ print("training on full data")
 # fit the best model on the full data
 best_model.fit(X, y)
 
-print("prediction"
+print("prediction")
 # do a prediction and save it
 pred = best_model.predict_proba(X_test)[:, 1]
 testfile = p.read_csv('./test.csv', sep=',', na_values=['?'], index_col=[0, 1])
@@ -214,6 +199,9 @@ testindices = [100*D + StId for (D, StId) in testfile.index]
 
 pred_df = p.DataFrame(np.vstack((testindices, pred))
                       .transpose(), columns=["Id", "Prediction"])
-pred_df.to_csv('./predictions/' + modelname + '/' + modelname + ' ' + strftime("%m-%d %X") + " C-" + str(round(best_C,4)) + " CV-" + str(round(best_cv, 4)) + ".csv", index = False)
+filename = './predictions/' + modelname + '/' + modelname + ' ' + \
+           strftime('%m-%d %X') + ' C-' + str(round(best_C, 4)) + \
+           ' CV-'+str(round(best_cv, 4))+'.csv'
+pred_df.to_csv(filename, index=False)
 
-print("submission file created") 
+print("submission file created")
